8836611e0da6b3587415aaa9b67a536527401a46
Salman Arefin <115951122+salmanarefin@users.noreply.github.com>
2022-11-08 01:30:40 +0600

    Add files via upload

---
+++ TASK 5.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=0,j=0;
+    int Array_1[10] = {8,4,6,1,6,9,6,1,8,8};
+
+    for(int i=0;i<10;i++)
+    {
+        for( j=0;j<10;j++)
+        {
+          if(Array_1[i]==Array_1[j])
+            {
+            count++;
+            }
+        }
+        for(j=i+1;j<10;j++)
+        {
+          if(Array_1[i]==Array_1[j])
+          {
+              break;
+          }
+        }
+        if(j==10)
+          cout<<Array_1[i]<<" occurs "<<count<<" times"<<endl;
+
+        count=0;
+    }
+}
+
